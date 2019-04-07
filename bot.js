39bcac6e2cc61345b6a3185019b194c590d15fd5
ayman2almonster <42792992+ayman2almonster@users.noreply.github.com>
2019-04-07 22:45:49 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,31 +8,4 @@ client.on('ready', () => {
 
    var prefix = "$";
 
-client.on('message', message => {
-    if(message.content.startsWith(prefix+'support')) {
-  const embed = new Discord.RichEmbed()
-    .setTitle("Invite Link")
-    .setDescription(`suport server **[here](https://discord.gg/MDXQZnJ)**`)
-    .setColor('RANDOM');
-    
-        message.author.send(embed)
-}
-
-  client.on('message',async message => {
-    if(message.content.startsWith(prefix + "restart")) {
-        if(message.author.id !== "375381620057964544") return message.reply('انت لست صاحب البوت!!!');
-        message.channel.send('**Restarting.**').then(msg => {
-            setTimeout(() => {
-               msg.edit('**Restarting..**');
-            },1000);
-            setTimeout(() => {
-               msg.edit('**Restarting...**');
-            },2000);
-        });
-        console.log(`${message.author.tag} [ ${message.author.id} ] تم إعادة تشغيل البوت بنجاح!`);
-        console.log(`Restarting..`);
-        setTimeout(() => {
-            client.destroy();
-            client.login('process.env.BOT_TOKEN');
-        },3000);
-    }
+client.login('process.env.BOT_TOKEN');
