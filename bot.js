beb2e77435d14929250a77322fb9fd1692abd89c
ayman2almonster <42792992+ayman2almonster@users.noreply.github.com>
2019-04-07 22:16:36 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,12 +3,8 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-});
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
+ 
+ console.log("Welcome Again !");
 
    var prefix = "$";
 
